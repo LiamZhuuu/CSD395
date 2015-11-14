caa6ca461f4f969d7e70202cbf2b978e297000dd
Jiaxu Zhu <jiaxuzhu92@gmail.com>
2015-11-13 16:26:08 -0800

    fully connected failed

--- src/autoEncoder.py
+++ src/autoEncoder.py
@@ -4,7 +4,12 @@ caffe.set_mode_gpu()
 
 solver = caffe.SGDSolver('/home/jiaxuzhu/developer/CSD395/models/autoencoder/md593_autoencoder_solver.prototxt')
 solver.solve()
-print solver.net.blobs['deconv2'].data[0][0]
+
+
+filters = solver.net.params['conv1'][0].data
+print filters
+img = solver.net.blobs['deconv1'].data[9].reshape(56,56) * 255
+cv2.imwrite('recon.jpg', img)
 #print net.blobs['data'].data
 
 '''
--- src/imageFormatting.py
+++ src/imageFormatting.py
@@ -10,7 +10,7 @@ img_path = img_dir + 'MD593_%04d_lossless_warped.tif' % (139)
 img = cv2.imread(img_path)
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-border_width = 112
+border_width = 28
 height, width, channels = img.shape
 #img_padded = cv2.copyMakeBorder(gray_img, border_width, border_width, border_width, border_width, cv2.BORDER_CONSTANT, value=0)
 img_padded = gray_img
@@ -30,23 +30,25 @@ txn_train = db_train.begin(write=True,buffers=True)
 txn_test = db_test.begin(write=True,buffers=True)
  
 count = 0
-for x in range(border_width, border_width+500, 10):
-    for y in range(border_width, border_width+500, 10):
-        patch = img_padded[y-border_width : y+border_width+1 ,x-border_width:x+border_width+1]
-        if count == 0:
-            cv2.imwrite('../images/md593_test.tif', patch)
+for x in range(border_width, border_width+1000, 10):
+    for y in range(border_width, border_width+1000, 10):
+        patch = img_padded[y-border_width : y+border_width ,x-border_width:x+border_width]
+        if count == 20:
+            cv2.imwrite('../images/md593_test.jpg', patch)
         # patch = patch[:, :, (2, 1, 0)]
         # patch = patch.transpose((2, 0, 1))
         
         datum = caffe.proto.caffe_pb2.Datum()
-        datum.height = border_width*2+1
-        datum.width = border_width*2+1
+        datum.height = border_width*2
+        datum.width = border_width*2
         datum.channels = 1
         datum.data = patch.tostring()
         
-        txn_train.put('%08d_%d_%d' % (count, x, y), datum.SerializeToString())
+        
         if ((y-border_width) % 20 == 0) or ((y-border_width) % 20 == 0):
             txn_test.put('%08d_%d_%d' % (count, x, y), datum.SerializeToString())
+        else:
+            txn_train.put('%08d_%d_%d' % (count, x, y), datum.SerializeToString())
         count = count + 1
         if count % 1000 == 0:
             # Write batch of images to database
