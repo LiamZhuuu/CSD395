ff5cc6d454eb7772fcfda5cd101bf45971923577
Jiaxu Zhu <jiaxuzhu92@gmail.com>
2016-01-18 09:55:07 -0800

    imagerec iter

---
+++ dataUtil.py
@@ -0,0 +1,75 @@
+import numpy as np
+import os
+import shutil
+import cv2
+
+def data_sampling(data_dir, dst_dir, p_labels, n_sample):
+
+    if os.path.exists(dst_dir):
+        shutil.rmtree(dst_dir)
+    os.mkdir(dst_dir)
+
+    train_list = open(os.path.join(dst_dir, 'train_list.txt'), 'w')
+    test_list = open(os.path.join(dst_dir, 'test_list.txt'), 'w')
+    eval_list = open(os.path.join(dst_dir, 'eval_list.txt'), 'w')
+
+    idx = 0
+    for j in range(len(p_labels)):
+            p_name = p_labels[j]
+            print p_name
+            sub_dst = os.path.join(dst_dir, p_name)
+            if os.path.exists(sub_dst):
+                shutil.rmtree(sub_dst)
+            os.mkdir(sub_dst)
+
+            patches = np.load(os.path.join(data_dir, '%s_patches.npy' % p_name))
+            print patches.shape
+            # patches = np.transpose(patches, (0, 3, 1, 2))
+            shuffle = np.random.permutation(len(patches))
+            patches = patches[shuffle[:n_sample], :]
+
+            for i in range(len(patches)):
+                image_path = os.path.join(sub_dst, '%s_%08d.jpg' % (p_name, i))
+                cv2.imwrite(image_path, patches[i, :])
+                if i < (n_sample / 2):
+                    train_list.write('%d\t%d\t%s\n' %(idx, j, '%s/%s_%08d.jpg' % (p_name, p_name, i)))
+                elif i < (n_sample / 4 * 3):
+                    eval_list.write('%d\t%d\t%s\n' %(idx, j, '%s/%s_%08d.jpg' % (p_name, p_name, i)))
+                else:
+                    test_list.write('%d\t%d\t%s\n' %(idx, j, '%s/%s_%08d.jpg' % (p_name, p_name, i)))
+                idx += 1
+    train_list.close()
+    eval_list.close()
+    test_list.close()
+
+# def data_loading(image_dir, p_labels, n_sample, ):
+
+
+
+data_dir = '/home/jiaxuzhu/data/landmark_patches'
+dst_dir = '/home/jiaxuzhu/data/sample_patches'
+p_labels = ['5N', '7n', '7N', '12N', 'Gr', 'LVe', 'Pn', 'SuVe', 'VLL', 'BackG']
+
+n_sample = 1024
+data_sampling(data_dir, dst_dir, p_labels, n_sample)
+
+# for root, dirs, files in os.walk(data_dir):
+#     break
+#
+# bg = None
+# print files
+# count = 0
+# for file in files:
+#     prefix = file.split('_')[0]
+#     # print prefix
+#     if prefix == 'patchesBg':
+#         tmp = np.load(os.path.join(data_dir, file))
+#         idx = np.random.permutation(len(tmp))[:10]
+#         tmp = tmp[idx, :]
+#         if bg is None:
+#             bg = tmp
+#         else:
+#             bg = np.vstack((bg, tmp))
+#         count += 1
+#         print count
+# np.save(os.path.join(data_dir, 'BackG_patches.npy'), bg)
--- texture/TextureClassifier.py
+++ texture/TextureClassifier.py
@@ -1,8 +1,11 @@
+import sys
+sys.path.insert(0, '/home/jiaxuzhu/developer/mxnet/python')
 import os
 import numpy as np
 import scipy
 import mxnet as mx
 import cv2
+
 
 def data_separation(data):
     n_data = len(data)
@@ -13,81 +16,36 @@ def data_separation(data):
 
 
 class TextureClassifier:
-    def __init__(self, data_dir, p_labels, n_sample):
-        self.patches = None
-        self.labels = None
-        self.n_class = len(p_labels)
-        idx = 0
-
-        for p_name in p_labels:
-            patches = np.load(os.path.join(data_dir, '%s_patches.npy' % p_name))
-            for patch in patches:
-                print np.max(patch), np.min(patch)
-                cv2.imshow('ImageWindow', patch)
-                cv2.waitKey()
-
-            patches = np.transpose(patches, (0, 3, 1, 2))
-            shuffle = np.random.permutation(len(patches))
-            patches = patches[shuffle[:n_sample], :]
-            patches = data_separation(patches)
-
-            labels = np.multiply(np.ones((n_sample, 1)), idx).astype(int)
-            labels = data_separation(labels)
-
-            if self.patches is None:
-                self.patches = patches
-                self.labels = labels
-            else:
-                for key in patches.keys():
-                    self.patches[key] = np.vstack((self.patches[key], patches[key]))
-                    self.labels[key] = np.vstack((self.labels[key], labels[key]))
-
-            idx += 1
-            print
-
-    def mx_init(self, model_dir, prefix, n_iter):
-        model = mx.model.FeedForward.load(os.path.join(model_dir, prefix), n_iter)
-        self.init_model = model
-        internals = model.symbol.get_internals()
-        print self.n_class
+    def __init__(self, data_dir, model_dir, prefix, n_iter, b_size, n_class):
+        sets = ['train', 'eval', 'test']
+        self.iter = {}
+        for dataset in sets:
+            rec_file = os.path.join(data_dir, '%s.rec' % dataset)
+            self.iter[dataset] = mx.io.ImageRecordIter(
+                path_imgrec=rec_file,
+                batch_size=b_size,
+                shuffle=True,
+                data_shape=(3, 224, 224)
+            )
+        self.init_model = mx.model.FeedForward.load(os.path.join(model_dir, prefix), n_iter)
+        internals = self.init_model.symbol.get_internals()
         symbol = internals['flatten_output']
-        symbol = mx.symbol.FullyConnected(data=symbol, name='fc1', num_hidden=self.n_class)
+        symbol = mx.symbol.FullyConnected(data=symbol, name='fullc', num_hidden=n_class)
         self.symbol = mx.symbol.SoftmaxOutput(data=symbol, name='softmax')
+        self.net = None
 
-        self.mxnet_mean = mx.nd.load(os.path.join(model_dir, 'mean_224.nd'))["mean_img"].\
-            asnumpy().reshape((1, 3, 224, 224))
-        for key in self.patches:
-            self.patches[key] = self.patches[key] - np.tile(self.mxnet_mean, (len(self.patches[key]), 1, 1, 1))
-
-    def mx_training(self, l_rate, b_size):
+    def mx_training(self, n_epoch, l_rate, b_size):
         opt = mx.optimizer.SGD(learning_rate=l_rate)
-        self.net = mx.model.FeedForward(ctx=mx.gpu(), symbol=self.symbol, num_epoch=5, optimizer=opt,
+        self.net = mx.model.FeedForward(ctx=mx.gpu(), symbol=self.symbol, num_epoch=n_epoch, optimizer=opt,
                                         arg_params=self.init_model.arg_params, aux_params=self.init_model.aux_params,
                                         allow_extra_params=True)
-        self.train_iter = mx.io.NDArrayIter(self.patches['train'], label=self.labels['train'].flatten(),
-                                       shuffle=True, batch_size=b_size)
+        self.net.fit(self.iter['train'], eval_data=self.iter['eval'],
+                     batch_end_callback=mx.callback.Speedometer(b_size, 30))
 
-        self.val_iter = mx.io.NDArrayIter(self.patches['val'], shuffle=True,
-                                     label=self.labels['val'].flatten(), batch_size=b_size)
-
-
-        self.net.fit(self.train_iter, eval_data=self.val_iter, batch_end_callback=mx.callback.Speedometer(b_size, 10))
-
-        self.train_iter.reset()
-        labels = self.net.predict(self.train_iter)
+        labels = self.net.predict(self.iter['train'])
+        print self.net.arg_params['fullc_weight']
         print labels[:10, :]
         labels = np.argmax(labels, axis=1)
         print labels
-        print self.labels['train'].flatten()
-        print float(len(np.where(labels == self.labels['train'].flatten())[0])) / len(labels)
 
         self.net.save('/home/jiaxuzhu/developer/CSD395/model/inception_texture')
-
-    def mx_predict(self, model_dir, prefix, n_iter):
-        model = mx.model.FeedForward.load(os.path.join(model_dir, prefix), n_iter, ctx=mx.gpu())
-        test_iter = mx.io.NDArrayIter(self.patches['test'], label=self.labels['test'].flatten(),
-                                      shuffle=True, batch_size=64)
-        train_iter = mx.io.NDArrayIter(self.patches['train'], label=self.labels['train'].flatten(),
-                                       shuffle=True, batch_size=64)
-        labels = np.argmax(model.predict(train_iter), axis=1)
-        print float(len(np.where(labels == self.labels['train'].flatten())[0])) / len(labels)
